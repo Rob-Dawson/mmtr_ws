4df7496927bdad8068a9f200cdd2facbfe81cba7
Rob Dawson <robdawson123.rd@gmail.com>
2025-03-11 12:18:27 +0000

    Added cmd_vel, odom and transforms to the ROS2_gz bridge. Added the odom_tf node to the launch file. This takes the gz odom and publishes a transform enabling RVIZ2 support Included the robot_localization package for EKF sensor fusion Added the necessary imu republisher for the EKF

--- src/mmtr_description/launch/gazebo.launch.py
+++ src/mmtr_description/launch/gazebo.launch.py
@@ -56,7 +56,10 @@ def generate_launch_description():
         executable="parameter_bridge",
         arguments=[
             "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
-            "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU"
+            "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU",
+            "/model/mmtr/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist",
+            "/model/mmtr/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry",
+            "/model/mmtr/tf@tf2_msgs/msg/TFMessage@gz.msgs/Pose_V",
         ],
         remappings=[
             ("/imu", "imu/out")
@@ -94,6 +97,39 @@ def generate_launch_description():
         ]
     )
 
+    odom_tf = Node(
+        package="mmtr_description",
+        executable="gz_tf_bridge.py",
+        output="screen"
+    )
+
+    static_transform_publisher = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=["--x", "0", "--y", "0","--z", "0.103",
+                   "--qx", "1", "--qy", "0", "--qz", "0", "--qw", "0",
+                   "--frame-id", "base_footprint_ekf",
+                   "--child-frame-id", "imu_link_ekf"],
+    )
+
+
+    robot_localization = Node(
+        package="robot_localization",
+        executable="ekf_node",
+        name="ekf_filter_node",
+        output="screen",
+        parameters=[os.path.join(get_package_share_directory("mmtr_description"), "config", "ekf.yaml")],
+    )
+
+
+    imu_republisher_cpp = Node(
+        package="mmtr_description",
+        executable="imu_republisher",
+    )
+
+
+
+
     ld = LaunchDescription()
     ld.add_action(model_arg)
     ld.add_action(world_arg)
@@ -105,6 +141,18 @@ def generate_launch_description():
     ld.add_action(joint_state_broadcaster_node)
     ld.add_action(flipper_controller)
     ld.add_action(gz_ros2_bridge)
+
+
+    ld.add_action(odom_tf)
+    ld.add_action(static_transform_publisher)
+    ld.add_action(robot_localization)
+    
+    ld.add_action(imu_republisher_cpp)
+
+
+
+
+
     # ld.add_action(custom_pid_node)
 
 
