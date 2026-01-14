f1ce1f1fe30820d43f0579ce8bd2e374f2e869dd
Rob-Dawson <robdawson123.rd@gmail.com>
2026-01-14 11:13:54 +0000

    ""

--- src/mmtr_local_mapping/launch/local_map.launch.py
+++ src/mmtr_local_mapping/launch/local_map.launch.py
@@ -1,14 +1,8 @@
 import os
 from ament_index_python import get_package_share_directory
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
-from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.actions import SetEnvironmentVariable
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import TimerAction
 
 
