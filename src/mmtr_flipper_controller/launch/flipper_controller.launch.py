b74031dfb82baaf32ee1ddf7d1797f8ce47d34e0
Rob-Dawson <robdawson123.rd@gmail.com>
2026-01-29 11:56:57 +0000

    Just for testing

--- src/mmtr_flipper_controller/launch/flipper_controller.launch.py
+++ src/mmtr_flipper_controller/launch/flipper_controller.launch.py
@@ -18,14 +18,14 @@ def generate_launch_description():
         executable="spawner",
         arguments=["flipper_controller", "--controller-manager", "/controller_manager"],
     )
-    flipper_default_pose = Node(
-        package="mmtr_flipper_controller",
-        executable="flipper_default_pos.py",
-    )
+    # flipper_default_pose = Node(
+    #     package="mmtr_flipper_controller",
+    #     executable="flipper_default_pos.py",
+    # )
 
     ld = LaunchDescription()
     ld.add_action(joint_state_broadcaster_node)
     ld.add_action(flipper_controller)
-    ld.add_action(flipper_default_pose)
+    # ld.add_action(flipper_default_pose)
 
     return ld
