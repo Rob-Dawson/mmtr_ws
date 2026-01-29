c35e71bc0adb72c2aa33854796554400c4b1eaba
Rob-Dawson <robdawson123.rd@gmail.com>
2026-01-29 11:58:09 +0000

    ""

--- src/mmtr_localisation/launch/localisation.launch.py
+++ src/mmtr_localisation/launch/localisation.launch.py
@@ -1,14 +1,7 @@
 import os
 from ament_index_python import get_package_share_directory
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
-from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.actions import SetEnvironmentVariable
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import TimerAction
 
 
@@ -36,7 +29,7 @@ def generate_launch_description():
 
         )
     
-    zupt = Node(package="mmtr_localisation", executable="zupt.py", parameters=[{"use_sim_time":True}])
+    # zupt = Node(package="mmtr_localisation", executable="zupt.py", parameters=[{"use_sim_time":True}])
     zero_yaw = Node(package="mmtr_localisation", executable="zero_yaw.py", parameters=[{"use_sim_time":True}])
     
     robot_localization_madgwick_ukf = Node(
@@ -59,7 +52,7 @@ def generate_launch_description():
     ld.add_action(imu_calibration)
     ld.add_action(mag_frame_conversion)
     ld.add_action(madgwick_filter)
-    ld.add_action(zupt)
+    # ld.add_action(zupt)
     ld.add_action(zero_yaw)
     ld.add_action(TimerAction(period=15.0, actions=[robot_localization_madgwick_ukf]))
 
