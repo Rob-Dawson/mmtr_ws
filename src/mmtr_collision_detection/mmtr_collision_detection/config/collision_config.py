80f7f13d0aa58c79a9c0cd39412d4491bd704ed2
Rob-Dawson <robdawson123.rd@gmail.com>
2026-05-21 20:21:09 +0100

    Added default values

--- src/mmtr_collision_detection/mmtr_collision_detection/config/collision_config.py
+++ src/mmtr_collision_detection/mmtr_collision_detection/config/collision_config.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python3
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class DirectionWeights:
-    yaw              :float
-    pitch            :float
-    axis             :float
-    dominant_axis    :float
+    yaw              :float = 0.0
+    pitch            :float = 0.0
+    axis             :float = 0.0
+    dominant_axis    :float = 0.0
 
 @dataclass
 class Thresholds:
-    yaw: float
-    pitch: float
-    ix: float
-    iy: float
+    yaw: float      = 0.0
+    pitch: float    = 0.0
+    ix: float       = 0.0
+    iy: float       = 0.0
+
+
 
 @dataclass
 class Weights:
-    front_rear: DirectionWeights
-    left_right: DirectionWeights
+    front_rear: DirectionWeights = field(default_factory=DirectionWeights)
+    left_right: DirectionWeights = field(default_factory=DirectionWeights)
 
 @dataclass
 class Config:
-    thresholds: Thresholds
-    weights: Weights
+    thresholds: Thresholds  = field(default_factory=Thresholds)
+    weights: Weights        = field(default_factory=Weights)
 
 @dataclass
 class ImuSample:
-    t_ns: int
-    jerk_mag: float
-    jerk_x: float
-    jerk_y: float
-    jerk_z: float
+    t_ns: int       = 0
+    jerk_mag: float = 0.0
+    jerk_x: float   = 0.0
+    jerk_y: float   = 0.0
+    jerk_z: float   = 0.0
